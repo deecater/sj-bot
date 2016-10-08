081cc57b2f57dee717d36147447e8934805f3dbb
deecater <dee@deecater.com>
2016-10-08 17:26:41 -0400

    took out sup

--- app.py
+++ app.py
@@ -39,10 +39,7 @@ def webhook():
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]  # the message's text
 
-                    if message_text = "sup":
-                        send_message(sender_id, "hi, there!")
-                    else:
-                        send_message(sender_id, "got it, thanks!")
+                    send_message(sender_id, "got it, thanks!")
 
                 if messaging_event.get("delivery"):  # delivery confirmation
                     pass
