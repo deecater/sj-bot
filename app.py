89df6467faecc6cdc0d7cc2517e2e65c242150ee
deecater <dee@deecater.com>
2016-10-08 20:37:35 -0400

    hi there

--- app.py
+++ app.py
@@ -33,16 +33,13 @@ def webhook():
         for entry in data["entry"]:
             for messaging_event in entry["messaging"]:
 
-                if 'sup' in messaging_event.get("message"):  # someone sent us a message
+                if messaging_event.get("message"):  # someone sent us a message
 
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]  # the message's text
 
                     send_message(sender_id, "hi, there!")
-
-                else:
-                    send_message(sender_id, "got it, thanks!")
 
                 if messaging_event.get("delivery"):  # delivery confirmation
                     pass
