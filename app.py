21262340f53014874c2f331435787fcb05970038
deecater <dee@deecater.com>
2016-10-09 09:12:17 -0400

    separated quick replies into separate file

--- app.py
+++ app.py
@@ -7,34 +7,37 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-# quick reply dictionaries
-fem_dictionary = [
-            {"content_type":"text",
-            "title":"What is feminism?",
-            "payload":"fem_definition"
-            },
-            {"content_type":"text",
-            "title":"History of feminism",
-            "payload":"fem_history"
-            }]
+#import quick reply dictionaries
+from quickReplyDicts import *
 
-start_dictionary = [
-            {"content_type":"text",
-            "title":"Feminism",
-            "payload":"start_fem"
-            },
-            {"content_type":"text",
-            "title":"Racism",
-            "payload":"start_race"
-            },
-            {"content_type":"text",
-            "title":"Gay Rights",
-            "payload":"start_gay"
-            },
-            {"content_type":"text",
-            "title":"Trans Rights",
-            "payload":"start_trans"
-            }]
+# quick reply dictionaries
+#fem_dictionary = [
+#            {"content_type":"text",
+#            "title":"What is feminism?",
+#            "payload":"fem_definition"
+#            },
+#            {"content_type":"text",
+#            "title":"History of feminism",
+#            "payload":"fem_history"
+#            }]
+
+#start_dictionary = [
+#            {"content_type":"text",
+#            "title":"Feminism",
+#            "payload":"start_fem"
+#            },
+#            {"content_type":"text",
+#            "title":"Racism",
+#            "payload":"start_race"
+#            },
+#            {"content_type":"text",
+#            "title":"Gay Rights",
+#            "payload":"start_gay"
+#            },
+#            {"content_type":"text",
+#            "title":"Trans Rights",
+#            "payload":"start_trans"
+#            }]
 
 
 @app.route('/', methods=['GET'])
---
+++ quickReplyDicts.py
@@ -0,0 +1,83 @@
+#initial dictionary
+start_dictionary = [
+            {"content_type":"text",
+            "title":"Feminism",
+            "payload":"start_fem"
+            },
+            {"content_type":"text",
+            "title":"Racism",
+            "payload":"start_race"
+            },
+            {"content_type":"text",
+            "title":"Gay Rights",
+            "payload":"start_gay"
+            },
+            {"content_type":"text",
+            "title":"Trans Rights",
+            "payload":"start_trans"
+            }
+]
+
+#feminism dictionary
+fem_dictionary = [
+            {"content_type":"text",
+            "title":"What is feminism?",
+            "payload":"fem_definition"
+            },
+            {"content_type":"text",
+            "title":"History of feminism",
+            "payload":"fem_history"
+            },
+            {"content_type":"text",
+             "title":"What feminism isn't",
+             "payload":"not_feminism"
+            }
+]
+
+#racism dictionary
+race_dictionary = [
+            {"content_type":"text",
+            "title":"What is racism?",
+            "payload":"racism_definition"
+            },
+            {"content_type":"text",
+            "title":"History of racism",
+            "payload":"racism_history"
+            },
+            {"content_type":"text",
+             "title":"What racism isn't",
+             "payload":"not_racism
+            }
+]
+
+#gay rights dictionary
+fem_dictionary = [
+            {"content_type":"text",
+            "title":"What are gay rights",
+            "payload":"gay_definition"
+            },
+            {"content_type":"text",
+            "title":"History of gay rights",
+            "payload":"gay_history"
+            },
+            {"content_type":"text",
+             "title":"What gay rights aren't",
+             "payload":"not_gay"
+            }
+]
+
+#trans rights dictionary
+fem_dictionary = [
+            {"content_type":"text",
+            "title":"What is trans?",
+            "payload":"trans_definition"
+            },
+            {"content_type":"text",
+            "title":"History of trans",
+            "payload":"trans_history"
+            },
+            {"content_type":"text",
+             "title":"What trans isn't",
+             "payload":"not_trans"
+            }
+]
