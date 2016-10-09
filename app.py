584cbd7b8e644363ee071fe59ad3b3d67e34c850
Jessica Kanczura <jkanczura@albany.edu>
2016-10-09 09:36:25 -0400

    move to the bottom
    for aesthetic

--- app.py
+++ app.py
@@ -8,91 +8,6 @@ from flask import Flask, request
 app = Flask(__name__)
 
 
-#initial dictionary
-start_dictionary = [
-            {"content_type":"text",
-            "title":"Feminism",
-            "payload":"start_fem"
-            },
-            {"content_type":"text",
-            "title":"Racism",
-            "payload":"start_race"
-            },
-            {"content_type":"text",
-            "title":"Gay Rights",
-            "payload":"start_gay"
-            },
-            {"content_type":"text",
-            "title":"Trans Rights",
-            "payload":"start_trans"
-            }
-]
-
-#feminism dictionary
-fem_dictionary = [
-            {"content_type":"text",
-            "title":"What is feminism?",
-            "payload":"fem_definition"
-            },
-            {"content_type":"text",
-            "title":"History of feminism",
-            "payload":"fem_history"
-            },
-            {"content_type":"text",
-             "title":"What feminism isn't",
-             "payload":"not_feminism"
-            }
-]
-
-#racism dictionary
-race_dictionary = [
-            {"content_type":"text",
-            "title":"What is racism?",
-            "payload":"racism_definition"
-            },
-            {"content_type":"text",
-            "title":"History of racism",
-            "payload":"racism_history"
-            },
-            {"content_type":"text",
-             "title":"What racism isn't",
-             "payload":"not_racism
-            }
-]
-
-#gay rights dictionary
-gay_dictionary = [
-            {"content_type":"text",
-            "title":"What are gay rights",
-            "payload":"gay_definition"
-            },
-            {"content_type":"text",
-            "title":"History of gay rights",
-            "payload":"gay_history"
-            },
-            {"content_type":"text",
-             "title":"What gay rights aren't",
-             "payload":"not_gay"
-            }
-]
-
-#trans rights dictionary
-trans_dictionary = [
-            {"content_type":"text",
-            "title":"What is trans?",
-            "payload":"trans_definition"
-            },
-            {"content_type":"text",
-            "title":"History of trans",
-            "payload":"trans_history"
-            },
-            {"content_type":"text",
-             "title":"What trans isn't",
-             "payload":"not_trans"
-            }
-]
-
-
 @app.route('/', methods=['GET'])
 def verify():
     # when the endpoint is registered as a webhook, it must echo back
@@ -214,3 +129,87 @@ def log(message):  # simple wrapper for logging to stdout on heroku
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+#initial dictionary
+start_dictionary = [
+            {"content_type":"text",
+            "title":"Feminism",
+            "payload":"start_fem"
+            },
+            {"content_type":"text",
+            "title":"Racism",
+            "payload":"start_race"
+            },
+            {"content_type":"text",
+            "title":"Gay Rights",
+            "payload":"start_gay"
+            },
+            {"content_type":"text",
+            "title":"Trans Rights",
+            "payload":"start_trans"
+            }
+]
+
+#feminism dictionary
+fem_dictionary = [
+            {"content_type":"text",
+            "title":"What is feminism?",
+            "payload":"fem_definition"
+            },
+            {"content_type":"text",
+            "title":"History of feminism",
+            "payload":"fem_history"
+            },
+            {"content_type":"text",
+             "title":"What feminism isn't",
+             "payload":"not_feminism"
+            }
+]
+
+#racism dictionary
+race_dictionary = [
+            {"content_type":"text",
+            "title":"What is racism?",
+            "payload":"racism_definition"
+            },
+            {"content_type":"text",
+            "title":"History of racism",
+            "payload":"racism_history"
+            },
+            {"content_type":"text",
+             "title":"What racism isn't",
+             "payload":"not_racism
+            }
+]
+
+#gay rights dictionary
+gay_dictionary = [
+            {"content_type":"text",
+            "title":"What are gay rights",
+            "payload":"gay_definition"
+            },
+            {"content_type":"text",
+            "title":"History of gay rights",
+            "payload":"gay_history"
+            },
+            {"content_type":"text",
+             "title":"What gay rights aren't",
+             "payload":"not_gay"
+            }
+]
+
+#trans rights dictionary
+trans_dictionary = [
+            {"content_type":"text",
+            "title":"What is trans?",
+            "payload":"trans_definition"
+            },
+            {"content_type":"text",
+            "title":"History of trans",
+            "payload":"trans_history"
+            },
+            {"content_type":"text",
+             "title":"What trans isn't",
+             "payload":"not_trans"
+            }
+]
