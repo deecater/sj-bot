5ce19a7c1e3f576ddecafe16fb870e229866481b
Jessica Kanczura <jkanczura@albany.edu>
2016-10-09 09:35:20 -0400

    Put dictionaries in main file
    it didn't work in its own file for some reason

--- app.py
+++ app.py
@@ -7,37 +7,90 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-#import quick reply dictionaries
-import quickReplyDicts
 
-# quick reply dictionaries
-#fem_dictionary = [
-#            {"content_type":"text",
-#            "title":"What is feminism?",
-#            "payload":"fem_definition"
-#            },
-#            {"content_type":"text",
-#            "title":"History of feminism",
-#            "payload":"fem_history"
-#            }]
+#initial dictionary
+start_dictionary = [
+            {"content_type":"text",
+            "title":"Feminism",
+            "payload":"start_fem"
+            },
+            {"content_type":"text",
+            "title":"Racism",
+            "payload":"start_race"
+            },
+            {"content_type":"text",
+            "title":"Gay Rights",
+            "payload":"start_gay"
+            },
+            {"content_type":"text",
+            "title":"Trans Rights",
+            "payload":"start_trans"
+            }
+]
 
-#start_dictionary = [
-#            {"content_type":"text",
-#            "title":"Feminism",
-#            "payload":"start_fem"
-#            },
-#            {"content_type":"text",
-#            "title":"Racism",
-#            "payload":"start_race"
-#            },
-#            {"content_type":"text",
-#            "title":"Gay Rights",
-#            "payload":"start_gay"
-#            },
-#            {"content_type":"text",
-#            "title":"Trans Rights",
-#            "payload":"start_trans"
-#            }]
+#feminism dictionary
+fem_dictionary = [
+            {"content_type":"text",
+            "title":"What is feminism?",
+            "payload":"fem_definition"
+            },
+            {"content_type":"text",
+            "title":"History of feminism",
+            "payload":"fem_history"
+            },
+            {"content_type":"text",
+             "title":"What feminism isn't",
+             "payload":"not_feminism"
+            }
+]
+
+#racism dictionary
+race_dictionary = [
+            {"content_type":"text",
+            "title":"What is racism?",
+            "payload":"racism_definition"
+            },
+            {"content_type":"text",
+            "title":"History of racism",
+            "payload":"racism_history"
+            },
+            {"content_type":"text",
+             "title":"What racism isn't",
+             "payload":"not_racism
+            }
+]
+
+#gay rights dictionary
+gay_dictionary = [
+            {"content_type":"text",
+            "title":"What are gay rights",
+            "payload":"gay_definition"
+            },
+            {"content_type":"text",
+            "title":"History of gay rights",
+            "payload":"gay_history"
+            },
+            {"content_type":"text",
+             "title":"What gay rights aren't",
+             "payload":"not_gay"
+            }
+]
+
+#trans rights dictionary
+trans_dictionary = [
+            {"content_type":"text",
+            "title":"What is trans?",
+            "payload":"trans_definition"
+            },
+            {"content_type":"text",
+            "title":"History of trans",
+            "payload":"trans_history"
+            },
+            {"content_type":"text",
+             "title":"What trans isn't",
+             "payload":"not_trans"
+            }
+]
 
 
 @app.route('/', methods=['GET'])
@@ -78,11 +131,11 @@ def webhook():
                     elif ("okay" in message_text):
                         send_message(sender_id, "affirmative")
                     elif (message_text.lower() == "feminism"):
-                        send_quick_reply(sender_id, "Awesome! Let's get started. What would you like to explore about feminism?", quickReplyDicts.fem_dictionary)
+                        send_quick_reply(sender_id, "Awesome! Let's get started. What would you like to explore about feminism?", fem_dictionary)
                     elif (message_text == "What is feminism?"):
                         send_message(sender_id, "did we fix it?")
                     else:
-                        send_quick_reply(sender_id, "Hi! My name is Social Justice Bot, or SJ Bot for short. What topic would you like to learn more about?", quickReplyDicts.start_dictionary)
+                        send_quick_reply(sender_id, "Hi! My name is Social Justice Bot, or SJ Bot for short. What topic would you like to learn more about?", start_dictionary)
 
                 if messaging_event.get("delivery"):  # delivery confirmation
                     pass
