20fbd5b845d4df078ff0790f4908d46062009fa0
deecater <dee@deecater.com>
2016-10-08 22:51:36 -0400

    fixing quick reply response

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ def webhook():
                     sender_id = messaging_event ["sender"]["id"]
 
                     if (message_text == "definition"):
-                        send_message(send_id, "You want to know the definition!")
+                        send_message(sender_id, "You want to know the definition!")
 
     return "ok", 200
 
